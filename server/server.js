55b4b0f297a6f22b312bdb52298236065b10f024
Wisemonkey <Oranbusiness@gmail.com>
2017-06-30 20:14:56 -0700

    Added p5js sketch to express server
    setup sockets between webpage and server
    created interface to save json object to server
    
    on server side
    it prints out the json object

--- server/server.js
+++ server/server.js
@@ -2,6 +2,16 @@
 var PORT = 3000;
 
 
+
+
+
+
+
+/*
+----------------------
+
+----------------------
+*/
 /*
 
 import all the nessisary packages and 
@@ -43,3 +53,128 @@ var io = socket(server);
 
 console.log("JumpNotes Server Has Started!");
 console.log("Address : http://localhost:" + PORT);
+
+
+
+
+/*
+
+Create a socket that the client app connects to 
+and every other client can acess
+
+*/
+
+var clientSockets =  new Sockets();
+
+
+
+
+
+io.on('connection', newConnection);
+
+
+function newConnection(socket){
+    
+      clientSockets.add(socket);
+
+      socket.on("text", function(data){
+
+          printNote(data);
+
+
+        }
+      );
+}
+
+
+
+
+
+
+
+/*
+----------------------
+takes json object and saves it to the
+mongodb database
+----------------------
+*/
+function saveNotetoDatabase(){
+
+}
+
+
+
+
+
+
+
+
+/*
+----------------------
+takes in the json object 
+like this one
+{
+    "id": 1,
+    "title": "My first Awesome Note!",
+    "noteText": "Idea for greate new book title Brave New World"
+}
+
+output
+
+Note :
+My first Awesome Note!
+Idea for greate new book title Brave New World
+____________________________________
+----------------------
+*/
+function printNote(Note){
+  if(Note.title !== '' || Note.text !== ''){
+    console.log("");
+    console.log("Note : ");
+    console.log(Note.title);
+    console.log(Note.text);
+    console.log("____________________________________");
+  }
+}
+
+
+
+
+
+
+
+
+
+/*
+
+Small Sockets class the handless socket tags better
+for documenation
+//https://stackoverflow.com/questions/18654173/emiting-with-exclusion-in-socket-io
+
+
+*/
+function Sockets() {
+  this.list = [ ];
+}
+Sockets.prototype.add = function(socket) {
+  this.list.push(socket);
+
+  var self = this;
+  socket.on('disconnect', function() {
+    self.remove(socket);
+  });
+}
+Sockets.prototype.remove = function(socket) {
+  var i = this.list.indexOf(socket);
+  if (i != -1) {
+    this.list.splice(i, 1);
+  }
+}
+Sockets.prototype.emit = function(name, data, except) {
+  var i = this.list.length;
+  while(i--) {
+    if (this.list[i] != except) {
+      this.list[i].emit(name, data)
+    }
+  }
+}
